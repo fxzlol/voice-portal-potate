dc797feafade46036f0011ec42bcb083c017e0bd
fexes1ze <schbcxfcz@gmail.com>
2026-07-31 23:12:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,51 +7,135 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Обслуживаем статические файлы из корневой директории (там лежит index.html)
 app.use(express.static(__dirname));
-
-// На любой GET-запрос, кроме статики, отдаём index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Хранилище комнат
-const rooms = {};
+// Очередь ожидающих (socket.id)
+let waitingQueue = [];
+// Пары: { roomId: { user1, user2 } }
+let pairs = {};
+let roomCounter = 0;
 
 io.on('connection', (socket) => {
   console.log(`[${socket.id}] connected`);
 
-  socket.on('join-room', ({ room, username }) => {
-    socket.join(room);
-    if (!rooms[room]) rooms[room] = { users: {} };
-    rooms[room].users[socket.id] = { username, socketId: socket.id };
-
-    io.to(room).emit('room-users', rooms[room].users);
-    socket.emit('room-joined', { room, users: rooms[room].users });
-    console.log(`[${socket.id}] joined room ${room}`);
+  socket.on('start-search', () => {
+    if (waitingQueue.includes(socket.id)) return;
+    // Проверим, не в паре ли
+    for (let room in pairs) {
+      if (pairs[room].user1 === socket.id || pairs[room].user2 === socket.id) return;
+    }
+    waitingQueue.push(socket.id);
+    socket.emit('search-status', { status: 'waiting' });
+    tryMatch();
   });
 
+  function tryMatch() {
+    while (waitingQueue.length >= 2) {
+      const user1 = waitingQueue.shift();
+      const user2 = waitingQueue.shift();
+      const s1 = io.sockets.sockets.get(user1);
+      const s2 = io.sockets.sockets.get(user2);
+      if (!s1 || !s2) {
+        if (s1) waitingQueue.unshift(user1);
+        if (s2) waitingQueue.unshift(user2);
+        continue;
+      }
+      const roomId = `room_${roomCounter++}`;
+      pairs[roomId] = { user1, user2 };
+      s1.emit('matched', { partnerId: user2, room: roomId });
+      s2.emit('matched', { partnerId: user1, room: roomId });
+      console.log(`[${user1}] matched with [${user2}] in ${roomId}`);
+    }
+  }
+
+  // Сигнальные сообщения (только для пары)
   socket.on('offer', ({ to, offer, room }) => {
-    io.to(to).emit('offer', { from: socket.id, offer, room });
+    if (pairs[room] && (pairs[room].user1 === socket.id || pairs[room].user2 === socket.id)) {
+      io.to(to).emit('offer', { from: socket.id, offer, room });
+    }
   });
 
   socket.on('answer', ({ to, answer, room }) => {
-    io.to(to).emit('answer', { from: socket.id, answer, room });
+    if (pairs[room] && (pairs[room].user1 === socket.id || pairs[room].user2 === socket.id)) {
+      io.to(to).emit('answer', { from: socket.id, answer, room });
+    }
   });
 
   socket.on('ice-candidate', ({ to, candidate, room }) => {
-    io.to(to).emit('ice-candidate', { from: socket.id, candidate, room });
+    if (pairs[room] && (pairs[room].user1 === socket.id || pairs[room].user2 === socket.id)) {
+      io.to(to).emit('ice-candidate', { from: socket.id, candidate, room });
+    }
+  });
+
+  // Скип
+  socket.on('skip', () => handleSkip(socket.id));
+
+  function handleSkip(socketId) {
+    let foundRoom = null, partnerId = null;
+    for (let room in pairs) {
+      if (pairs[room].user1 === socketId) {
+        foundRoom = room;
+        partnerId = pairs[room].user2;
+        break;
+      } else if (pairs[room].user2 === socketId) {
+        foundRoom = room;
+        partnerId = pairs[room].user1;
+        break;
+      }
+    }
+    if (!foundRoom) return;
+    delete pairs[foundRoom];
+
+    const s1 = io.sockets.sockets.get(socketId);
+    const s2 = io.sockets.sockets.get(partnerId);
+    if (s1) {
+      s1.emit('partner-disconnected');
+      waitingQueue.push(socketId);
+      s1.emit('search-status', { status: 'waiting' });
+    }
+    if (s2) {
+      s2.emit('partner-disconnected');
+      waitingQueue.push(partnerId);
+      s2.emit('search-status', { status: 'waiting' });
+    }
+    tryMatch();
+  }
+
+  socket.on('stop-search', () => {
+    const idx = waitingQueue.indexOf(socket.id);
+    if (idx !== -1) waitingQueue.splice(idx, 1);
+    handleSkip(socket.id);
+    socket.emit('search-status', { status: 'idle' });
   });
 
   socket.on('disconnect', () => {
-    for (const room in rooms) {
-      if (rooms[room].users[socket.id]) {
-        delete rooms[room].users[socket.id];
-        io.to(room).emit('room-users', rooms[room].users);
-        if (Object.keys(rooms[room].users).length === 0) {
-          delete rooms[room];
-        }
+    const idx = waitingQueue.indexOf(socket.id);
+    if (idx !== -1) waitingQueue.splice(idx, 1);
+    let foundRoom = null, partnerId = null;
+    for (let room in pairs) {
+      if (pairs[room].user1 === socket.id) {
+        foundRoom = room;
+        partnerId = pairs[room].user2;
         break;
+      } else if (pairs[room].user2 === socket.id) {
+        foundRoom = room;
+        partnerId = pairs[room].user1;
+        break;
+      }
+    }
+    if (foundRoom) {
+      delete pairs[foundRoom];
+      if (partnerId) {
+        const ps = io.sockets.sockets.get(partnerId);
+        if (ps) {
+          ps.emit('partner-disconnected');
+          waitingQueue.push(partnerId);
+          ps.emit('search-status', { status: 'waiting' });
+          tryMatch();
+        }
       }
     }
     console.log(`[${socket.id}] disconnected`);
@@ -59,6 +143,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
